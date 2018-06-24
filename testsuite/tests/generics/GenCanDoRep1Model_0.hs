991b81b5354408ceae88836fca88f721e5c73bb1
Cedric Shock <cedricshock@gmail.com>
2018-06-24 21:49:15 +0000

    Better test cases for Generic1, separated by cause of failure

--- testsuite/tests/generics/GenCanDoRep1Model_0.hs
+++ testsuite/tests/generics/GenCanDoRep1Model_0.hs
@@ -9,7 +9,6 @@ import GHC.Generics (Generic, Generic1)
 
 -- We should be able to generate a generic representation for these models
 
-
 data Parent f = Parent {
     name :: f String,
     child :: f (Child f)
@@ -22,10 +21,10 @@ data Child f = Child {
 }
   deriving (Generic1)
 
+data Fix f = In f (Fix f)                                   deriving Generic1
+
 data ListF a f = NilF | ConsF (f a) (f (ListF a f))         deriving Generic1
 
 data RoseF a f = RoseF a [f (RoseF a f)]                    deriving Generic1
 
 data ParAp0 c p = ParAp0 (p c)                              deriving Generic1
-
-data ParAp1 f p = ParAp1 (p (f p))                          deriving Generic1
---
+++ testsuite/tests/generics/GenCanDoRep1Model_1.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
+
+module CanDoRep1Model where
+
+import GHC.Generics (Generic, Generic1)
+
+
+-- We should be able to generate a generic representation for ParAp1
+
+data ParAp1 f p = ParAp1 (p (f p))                     deriving Generic1
